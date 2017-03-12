7ed5148dcbe9ac7c5c144c0f0127c55cbb798ffa
PhysicsMathsMan <mr.adithya.atg@gmail.com>
2017-03-12 14:59:14 +0530

    Editing 1

--- ex1/ex1.m
+++ ex1/ex1.m
@@ -26,6 +26,8 @@
 %% Initialization
 clear ; close all; clc
 
+
+
 %% ==================== Part 1: Basic Function ====================
 % Complete warmUpExercise.m 
 %fprintf('Running warmUpExercise ... \n');
@@ -36,24 +38,30 @@ clear ; close all; clc
 %pause;
 
 
+
+
 %% ======================= Part 2: Plotting =======================
 fprintf('Plotting given Data\n')
+
 data = load('ex1data1.txt');
 X = data(:, 1); y = data(:, 2);
 m = length(y); % number of training examples
 
 % Plot Data
-% Note: You have to complete the code in plotData.m
 plotData(X, y);
+
 
 fprintf('Program paused. Press enter to continue.\n');
 pause;
 
-%% =================== Part 3: Gradient descent ===================
-fprintf('Running Gradient Descent...\n')
 
-X = [ones(m, 1), data(:,1)];     % Add a column of ones to x
+
+%% =================== Part 3: Gradient descent ===================
+fprintf('Running Gradient Descent.\n')
+
+X = [ones(m, 1), data(:,1)];     % Add a column of ones to x; Note that we use data(:,1) to write the secoond column
 theta = zeros(2, 1);             % initialize fitting parameters
+
 
 % Some gradient descent settings
 iterations = 1500;
